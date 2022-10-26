60bc7dc9df655e576199e70a82abc5a8f37c5310
Joshua Nario <joshuanario@users.noreply.github.com>
2022-10-26 00:34:28 -0400

    perf_eng prog

--- pages/perfeng.tsx
+++ pages/perfeng.tsx
@@ -1,13 +1,14 @@
 import path from 'path'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import {read, Compatible} from 'to-vfile'
+import {read, writeSync, Compatible} from 'to-vfile'
 import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remark from 'remark'
 import remarkMermaid from 'remark-mermaid'
 import remarkMath from 'remark-math'
 import remarkGfm from 'remark-gfm'
+import remarkEmbedImages from 'remark-embed-images'
 import remarkRehype from 'remark-rehype'
 import rehypeMathjax from 'rehype-mathjax'
 import rehypeStringify from 'rehype-stringify'
@@ -32,16 +33,22 @@ const PerfEng: NextPage<MyProps> = ({ content, }: MyProps )  => {
 // This function gets called at build time
 export async function getStaticProps() {
     const mdpath = path.resolve(process.cwd(), 'public', 'perf_eng.md')
-    const outroot = path.resolve(process.cwd(), 'out')
-    const targetfolder = path.resolve(process.cwd(), 'out', 'perfeng')
+    const targetfolder = path.resolve(process.cwd(), '_mmd')
     const mmdtarget = await read(mdpath)
     mmdtarget.data = {
         destinationDir: targetfolder
     }
-    const mmdcontent = await remark().use(remarkMermaid).process(mmdtarget)
+    await remark().use(remarkMermaid).process(mmdtarget, function (err, file) {
+      if (err) throw err;
+      const desc = file as any
+      desc.path = path.resolve(process.cwd(), '_mmd', 'perf_eng.md')
+      writeSync(desc as Compatible);
+    })
 
+    const mmdcontent = await read(path.resolve(process.cwd(), '_mmd', 'perf_eng.md'))
     const file = await unified()
     .use(remarkParse)
+    .use(remarkEmbedImages)
     .use(remarkGfm)
     .use(remarkMath)
     .use(remarkRehype)
