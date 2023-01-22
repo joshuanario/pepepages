be3fa1fee0b4b4e8a6ce211352237454edc3a0a7
Joshua Nario <joshuanario@users.noreply.github.com>
2023-01-22 18:08:44 -0500

    synopsis reworked

--- components/PastLife/index.tsx
+++ components/PastLife/index.tsx
@@ -4,18 +4,21 @@ import 'normalize.css/normalize.css'
 
 type MyProps = {
   moreinfo: string
-  synopsis: string
   videoUrl: string
 }
 
 const PastLife: React.FC<MyProps> = (props: MyProps) => {
-  const { moreinfo, synopsis, videoUrl } = props
+  const { moreinfo, videoUrl } = props
   return <div id={styles.container}>
     <div id={styles.wrapper}>
       <div id={styles.top}>
-        <div dangerouslySetInnerHTML={{
-            __html: synopsis,
-        }}/>
+        <div>
+          <h1>Film-making</h1>
+          <p>In a past life, I aspired to make films.  The video in this page was probably completed around 2006-2007.</p>
+          <h2>Siege on Lochaven Hills</h2>
+          <p>Synopsis: Jeff and Roi decided to rob Ha's house, but things did not go as they planned when they arrived at their destination.</p>
+          <p>Disclaimer: Viewer discretion is advised due to comical violence and mild gore.</p>
+        </div>
         <div>
           <video width="100%" height="100%" controls>
             <source src={videoUrl} type="video/ogg" />
--- pages/pastlife.tsx
+++ pages/pastlife.tsx
@@ -9,20 +9,13 @@ type MyProps = {
 }
 
 const PastLifePage: NextPage<MyProps> = ({ moreinfo }: MyProps ) => {
-  const synopsis = `
-  <h1>Film-making</h1>
-  <p>In a past life, I aspired to make films.  The video in this page was probably completed around 2006-2007.<p>
-  <h2>Siege on Lochaven Hills</h2>
-  <p>Synopsis: Jeff and Roi decided to rob Ha's house, but things did not go as they planned when they arrived at their destination.</p>
-  <p>Disclaimer: Viewer discretion is advised due to comical violence and mild gore.</p>
-  `
   return (
     <>
       <Head>
         <title>In a past life</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <PastLife synopsis={synopsis} moreinfo={moreinfo} videoUrl='/sdsf_ch09.ogg'/>
+      <PastLife moreinfo={moreinfo} videoUrl='/sdsf_ch09.ogg'/>
     </>
   )
 }
